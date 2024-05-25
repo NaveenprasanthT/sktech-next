fc4000296c374e3dd6ec730d480005334e2b58c6
Naveenprasanth07 <naveen.t@in.aictec.com>
2024-05-25 10:39:37 +0530

    product-page

---
+++ src/components/Product/ProductBanner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from '@/styles/Product/productBanner.module.css';
+import SharedButton from '../Shared/Button';
+import Image from 'next/image';
+
+function ProductBanner(props) {
+  const { bannerContent } = props;
+  return (
+    <div className={styles.productBannerWrap}>
+      <div className={styles.productBannerDetails}>
+        <div className={styles.productBannerContent}>
+          <span>{bannerContent?.smallTitle}</span>
+          <h1>{bannerContent?.title}</h1>
+          <p>{bannerContent?.description}</p>
+        </div>
+        {bannerContent?.buttonLabel
+          && (
+            <SharedButton
+              label={bannerContent?.buttonLabel}
+              buttonClick={bannerContent?.buttonClick}
+            />
+          )}
+      </div>
+      <Image
+        src={bannerContent?.imagePath}
+        alt="Product image"
+        width={1000}
+        height={1000}
+        className={styles.productBannerImage}
+      />
+    </div>
+  );
+}
+
+export default ProductBanner;
