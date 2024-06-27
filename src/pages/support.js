3a5317130d892be7950d88cfd987de6b5a9e8980
mownika01 <mownika3740@gmail.com>
2024-06-27 22:15:04 +0530

    support page

---
+++ src/pages/support.js
@@ -0,0 +1,16 @@
+import Footer from '@/components/Shared/Footer'
+import Navbar from '@/components/Shared/Navbar'
+import Support from '@/components/Support/Support'
+import React from 'react'
+
+const support = () => {
+  return (
+    <div>
+        <Navbar />
+        <Support />
+        <Footer />
+    </div>
+  )
+}
+
+export default support;
